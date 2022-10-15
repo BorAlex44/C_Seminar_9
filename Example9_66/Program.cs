b6d508ccdc55be28d3e995f4fc88d8768ed059da
BorAlex <alexander44@bl.ru>
2022-10-15 17:01:28 +0300

    add Example9_66

---
+++ Example9_66/Program.cs
@@ -0,0 +1,15 @@
+﻿using static System.Console;
+Clear();
+Write("Введите первое число: ");
+int num1 = int.Parse(ReadLine()!);
+Write("Введите второе число: ");
+int num2 = int.Parse(ReadLine()!);
+WriteLine($"Сумма чисел в диапазоне от {num1} до {num2} равна {GetNumberSumma(num1, num2)}");
+
+
+
+int GetNumberSumma(int m, int n)
+{
+    if (m == n) return n;
+    return n + GetNumberSumma(m, n - 1);
+}
