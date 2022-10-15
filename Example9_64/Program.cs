1f1e732fd9bb74366c714b5379e82bce300aa470
BorAlex <alexander44@bl.ru>
2022-10-15 16:42:33 +0300

    add Example9_64

--- Example9_64/Program.cs
+++ Example9_64/Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using static System.Console;
+Clear();
+Write("Введите число n > 0: ");
+int number = int.Parse(ReadLine()!);
+GetNumbers(number);
+
+
+
+void GetNumbers(int n)
+{
+    if (n == 1) WriteLine(n);
+    else
+    {
+        Write($"{n} ");
+        GetNumbers(n - 1);
+    }
+}
